14ef2d4cf1aa18bd40b259268883b7cbe76eb6ff
zhangxinpeng <zh773881228@163.com>
2024-05-05 21:54:36 +0800

    0505 单账号免找点部署测试优化

--- aixing_tools.py
+++ aixing_tools.py
@@ -4,7 +4,9 @@ import os
 import pyautogui
 import cv2
 import numpy as np
+from datetime import datetime
 from paddleocr import PaddleOCR
+test_debug = 1
 ocr = PaddleOCR(use_angle_cls=True, lang="ch")  # need to run only once to download and load model into memory
 width = 1920
 height = 1080
@@ -18,7 +20,6 @@ def InferOcrApp(x1=0, y1=0, x2=width, y2=height):
     img_path = np.array(app_image)
     result = ocr.ocr(img_path, cls=True)
     return result
-
 
 def calculate_center(points):
     # 假设 points 是按照顺时针或逆时针顺序给出的四个点
@@ -56,8 +57,8 @@ def find_image_in_large(large_image, small_image_name, threshold=0.85, type="poi
     # 读取大图和小图
     large_image = cv2.cvtColor(large_image, cv2.COLOR_BGR2GRAY)
     small_image = cv2.imread(small_image_path, 0)
-    cv2.imwrite("large.png", large_image)
-    cv2.imwrite("small.png", small_image)
+    # cv2.imwrite("large.png", large_image)
+    # cv2.imwrite("small.png", small_image)
     if large_image is None or small_image is None:
         print("Error loading images!")
         return None
@@ -69,8 +70,6 @@ def find_image_in_large(large_image, small_image_name, threshold=0.85, type="poi
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
 
     # 检查最大匹配值是否超过设定的阈值
-    print(max_val)
-    print(small_image.shape)
     if max_val >= threshold:
         if type == "point":
             return max_loc[0]+(small_image.shape[1]//2), max_loc[1]+(small_image.shape[0]//2)
@@ -82,6 +81,99 @@ def find_image_in_large(large_image, small_image_name, threshold=0.85, type="poi
 def calculate_text_difference(text1, text2):
     matcher = SequenceMatcher(None, text1, text2)
     return matcher.ratio()
+
+def Check_g_com(g_com, app_center_x, app_center_y):
+    pyautogui.moveTo(app_center_x, app_center_y)
+    scoll_time = 0
+    while True: # 添加循环，每次一循环滚轮滚一次
+        result = InferOcrApp()
+        for idx in range(len(result)):
+            res = result[idx]
+            for line in res:
+                if calculate_text_difference(g_com, line[-1][0]) >= 0.85:
+                    # 找到了，返回边框中心点
+                    return 1
+        if scoll_time > 4:
+            print(f"没有【{g_com}】这个商品！！")
+            pyautogui.moveTo(width / 2, height / 2)
+            pyautogui.scroll(500)
+            return 0
+        scoll_time += 1
+        pyautogui.moveTo(app_center_x, app_center_y)
+        pyautogui.scroll(-100)
+        time.sleep(1)
+
+def sell_function(coordinate_dict, g_time):
+    pyautogui.moveTo(coordinate_dict["委拍1point"][0], coordinate_dict["委拍1point"][1])
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.moveTo(coordinate_dict["全部point"][0], coordinate_dict["全部point"][1])
+    pyautogui.click()
+    time.sleep(1)
+    pyautogui.moveTo(coordinate_dict["委拍2point"][0], coordinate_dict["委拍2point"][1])
+    pyautogui.click()
+    time.sleep(1)
+    while True:
+        result = InferOcrApp(coordinate_dict["取消box"][0], coordinate_dict["取消box"][1], coordinate_dict["取消box"][2], coordinate_dict["取消box"][3])
+        try:
+            if result and result[0][0][1][0] == "取消":
+                pyautogui.moveTo(coordinate_dict["全部point"][0], coordinate_dict["全部point"][1])
+                pyautogui.click()
+                time.sleep(0.25)
+                pyautogui.moveTo(coordinate_dict["委拍2point"][0], coordinate_dict["委拍2point"][1])
+                pyautogui.click()
+                time.sleep(1)
+            else:
+                print("委拍完了！！！")
+                break
+        except:
+            print("委拍完了！")
+            break
+        if test_debug==1:
+            now = datetime.now()
+            hour = now.hour
+            minute = now.minute
+            second = now.second
+            trans_time = hour*60*60 + minute*60 + second
+            if abs(trans_time-g_time) > 300:
+                print("委拍超时")
+                return 0
+def buy_function(coordinate_dict, g_time):
+    pyautogui.click(coordinate_dict["参拍1point"][0], coordinate_dict["参拍1point"][1])
+    time.sleep(1)
+    pyautogui.click(coordinate_dict["全部point"][0], coordinate_dict["全部point"][1])
+    time.sleep(1)
+    pyautogui.click(coordinate_dict["参拍2point"][0], coordinate_dict["参拍2point"][1])
+    time.sleep(1)
+    while True:
+        result = InferOcrApp(coordinate_dict["取消box"][0], coordinate_dict["取消box"][1], coordinate_dict["取消box"][2], coordinate_dict["取消box"][3])
+        try:
+            if result and result[0][0][1][0] == "取消":
+                pyautogui.moveTo(coordinate_dict["全部point"][0], coordinate_dict["全部point"][1])
+                pyautogui.click()
+                time.sleep(0.25)
+                pyautogui.moveTo(coordinate_dict["参拍2point"][0], coordinate_dict["参拍2point"][1])
+                pyautogui.click()
+                time.sleep(1)
+            else:
+                print("抢完了！！！")
+                pyautogui.moveTo(coordinate_dict["返回point"][0], coordinate_dict["返回point"][1])
+                pyautogui.click()
+                return 1
+        except:
+            print("抢完了！")
+            pyautogui.moveTo(coordinate_dict["返回point"][0], coordinate_dict["返回point"][1])
+            pyautogui.click()
+            return 1
+        if test_debug == 1:
+            now = datetime.now()
+            hour = now.hour
+            minute = now.minute
+            second = now.second
+            trans_time = hour*60*60 + minute*60 + second
+            if abs(trans_time-g_time) > 300:
+                print("参拍超时")
+                return 0
 def set_coordinate():
     app_center_x = 0
     tmp_list = []
@@ -101,35 +193,57 @@ def set_coordinate():
         return None
     else:
         pyautogui.moveTo(point[0], point[1])
-        time.sleep(2)
+        time.sleep(1)
         app_center_x += point[0]//2
         f.write("我的point "+str(point[0])+","+str(point[1])+"\n")
         pyautogui.click()
-        time.sleep(2)
+        time.sleep(1)
 
     screenshot = np.array(pyautogui.screenshot())
     box = find_image_in_large(screenshot, "mysell", type="box")
-    print(box)
     if box is None:
         print("[我的委拍] 没找到")
         return None
     else:
         pyautogui.moveTo(box[0]+box[2]//2, box[1]+box[3])
-        time.sleep(2)
+        time.sleep(1)
         f.write("委拍数量box "+str(box[0])+","+str(box[1])+","+str(box[0]+box[2])+","+str(box[1]+box[3]*2)+"\n")
-        cv2.imwrite("test_weipai.png", screenshot[box[1]:box[1]+box[3]*2, box[0]:box[0]+box[2]])
 
+    point = find_image_in_large(screenshot, "mystore")
+    if point is None:
+        print("[我的仓库] 没找到")
+        return None
+    else:
+        pyautogui.moveTo(point[0], point[1])
+        time.sleep(1)
+        f.write("我的仓库point "+str(point[0])+","+str(point[1])+"\n")
+        pyautogui.click()
+        time.sleep(1)
+
+    screenshot = np.array(pyautogui.screenshot())
+    back_point = find_image_in_large(screenshot, "back")
+    if back_point is None:
+        print("[返回] 没找到")
+        return None
+    else:
+        pyautogui.moveTo(back_point[0], back_point[1])
+        time.sleep(1)
+        f.write("返回point "+str(back_point[0])+","+str(back_point[1])+"\n")
+        pyautogui.click()
+        time.sleep(1)
+
+    screenshot = np.array(pyautogui.screenshot())
     point = find_image_in_large(screenshot, "first")
     if point is None:
         print("[首页] 没找到")
         return None
     else:
         pyautogui.moveTo(point[0], point[1])
-        time.sleep(2)
+        time.sleep(1)
         f.write("首页point "+str(point[0])+","+str(point[1])+"\n")
         app_center_x += point[0] // 2
         pyautogui.click()
-        time.sleep(2)
+        time.sleep(1)
 
     # 进商品拍卖界面，确认其他点位
     for item in tmp_list:
@@ -149,7 +263,7 @@ def set_coordinate():
     pyautogui.moveTo(g_com_p[0], g_com_p[1])
     time.sleep(1)
     pyautogui.click()
-    time.sleep(2)
+    time.sleep(1)
     screenshot = np.array(pyautogui.screenshot())
     point = find_image_in_large(screenshot, "buy1")
     if point is None:
@@ -157,7 +271,7 @@ def set_coordinate():
         return None
     else:
         pyautogui.moveTo(point[0], point[1])
-        time.sleep(2)
+        time.sleep(1)
         f.write("参拍1point "+str(point[0])+","+str(point[1])+"\n")
     point = find_image_in_large(screenshot, "sell1")
     if point is None:
@@ -165,10 +279,10 @@ def set_coordinate():
         return None
     else:
         pyautogui.moveTo(point[0], point[1])
-        time.sleep(2)
+        time.sleep(1)
         f.write("委拍1point "+str(point[0])+","+str(point[1])+"\n")
         pyautogui.click()
-        time.sleep(2)
+        time.sleep(1)
     screenshot = np.array(pyautogui.screenshot())
     point = find_image_in_large(screenshot, "sell2")
     if point is None:
@@ -176,7 +290,7 @@ def set_coordinate():
         return None
     else:
         pyautogui.moveTo(point[0], point[1])
-        time.sleep(2)
+        time.sleep(1)
         f.write("委拍2point "+str(point[0])+","+str(point[1])+"\n")
         f.write("参拍2point " + str(point[0]) + "," + str(point[1]) + "\n")
     point = find_image_in_large(screenshot, "all")
@@ -185,7 +299,7 @@ def set_coordinate():
         return None
     else:
         pyautogui.moveTo(point[0], point[1])
-        time.sleep(2)
+        time.sleep(1)
         f.write("全部point "+str(point[0])+","+str(point[1])+"\n")
     point = find_image_in_large(screenshot, "cancel")
     box = find_image_in_large(screenshot, "cancel", type="box")
@@ -194,19 +308,17 @@ def set_coordinate():
         return None
     else:
         pyautogui.moveTo(point[0], point[1])
-        time.sleep(2)
+        time.sleep(1)
         f.write("取消point "+str(point[0])+","+str(point[1])+"\n")
         f.write("取消box "+str(box[0])+","+str(box[1])+","+str(box[0]+box[2])+","+str(box[1]+box[3])+"\n")
-        cv2.imwrite("test_weipai.png", screenshot[box[1]:box[1]+box[3], box[0]:box[0]+box[2]])
-    point = find_image_in_large(screenshot, "back")
-    if point is None:
-        print("[返回] 没找到")
-        return None
-    else:
-        pyautogui.moveTo(point[0], point[1])
-        time.sleep(2)
-        f.write("返回point "+str(point[0])+","+str(point[1])+"\n")
-        pyautogui.click()
-        time.sleep(2)
+        # cv2.imwrite("test_weipai.png", screenshot[box[1]:box[1]+box[3], box[0]:box[0]+box[2]])
+
+    pyautogui.moveTo(back_point[0], back_point[1])
+    time.sleep(1)
+    pyautogui.click()
     print("所有的点位都已找到")
     return app_center_x
+
+
+if __name__ == "__main__":
+    set_coordinate()
--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import numpy as np
 from datetime import datetime
 
 from aixing_tools import set_coordinate, InferOcrApp, calculate_text_difference, calculate_center, height, width
+from aixing_tools import Check_g_com, buy_function, sell_function
 
 shop_list = []
 coordinate_dict = {}
@@ -14,108 +15,77 @@ app_center_x = 0
 app_center_y = height//2
 def BuySellFunction(g_com, g_time, g_point, scroll_time):
     # 0. 进我的委拍，记录数量
-    print(coordinate_dict["我的point"])
     pyautogui.moveTo(coordinate_dict["我的point"][0], coordinate_dict["我的point"][1])
     pyautogui.click()
-    time.sleep(2)
+    time.sleep(1)
     # 截取委拍位置的图像
     result = InferOcrApp(coordinate_dict["委拍数量box"][0], coordinate_dict["委拍数量box"][1], coordinate_dict["委拍数量box"][2], coordinate_dict["委拍数量box"][3])
-    wei_pai_num = int(result[0][-1][1][0])
-    # 1. 先进委拍，看看数量是否为0
-    # 1.1 如果不为0， 进委拍， 点最大数量，一直点委派，每次点完看看有没有【取消】的字段，有就继续，没有就下一环节
-    # 1.2 如果为0，进参拍，点最大数量，一直参拍，每次点完看有没有取消字段，有就继续，没有就参拍完成，直接return
-    if wei_pai_num == 0:
-        print("当前委拍为0")
-        # 进首页
+    wei_pai_num_1 = int(result[0][-1][1][0])
+    pyautogui.click(coordinate_dict["我的仓库point"][0], coordinate_dict["我的仓库point"][1])
+    time.sleep(1)
+    # 1. 先进仓库，看有没有这个货品，有的话进委拍，没有的话进参拍
+    check_g_com_flag = Check_g_com(g_com, app_center_x, app_center_y)
+
+    pyautogui.click(coordinate_dict["返回point"][0], coordinate_dict["返回point"][1])
+    time.sleep(1)
+    if check_g_com_flag == 0: #没这个商品，进参拍
         pyautogui.moveTo(coordinate_dict["首页point"][0], coordinate_dict["首页point"][1])
         pyautogui.click()
         time.sleep(1)
         for i in range(0, scroll_time):
-            pyautogui.moveTo(width/2, height/2)
+            pyautogui.moveTo(app_center_x, app_center_y)
             pyautogui.scroll(-100)
         print(g_point)
         pyautogui.moveTo(g_point[0], g_point[1])
         pyautogui.click()
         time.sleep(1)
-        pyautogui.moveTo(coordinate_dict["参拍1point"][0], coordinate_dict["参拍1point"][1])
-        pyautogui.click()
-        time.sleep(1)
-        pyautogui.moveTo(coordinate_dict["全部point"][0], coordinate_dict["全部point"][1])
-        pyautogui.click()
-        time.sleep(1)
-        pyautogui.moveTo(coordinate_dict["参拍2point"][0], coordinate_dict["参拍2point"][1])
-        pyautogui.click()
-        time.sleep(2)
-        while True:
-            result = InferOcrApp(coordinate_dict["取消box"][0], coordinate_dict["取消box"][1], coordinate_dict["取消box"][2], coordinate_dict["取消box"][3])
-            try:
-                if result and result[0][0][1][0] == "取消":
-                    pyautogui.moveTo(coordinate_dict["全部point"][0], coordinate_dict["全部point"][1])
-                    pyautogui.click()
-                    time.sleep(0.25)
-                    pyautogui.moveTo(coordinate_dict["参拍2point"][0], coordinate_dict["参拍2point"][1])
-                    pyautogui.click()
-                    time.sleep(1)
-                else:
-                    print("抢完了！！！")
-                    return
-            except:
-                print("抢完了！")
-                return
-    else:
-        print("当前委拍为 "+str(wei_pai_num))
+        buy_function(coordinate_dict, g_time)
+        return
+
+    else: # 进委拍
+        print("当前委拍为 "+str(wei_pai_num_1))
         # 进首页
-        pyautogui.moveTo(coordinate_dict["首页point"][0], coordinate_dict["首页point"][1])
-        pyautogui.click()
+        pyautogui.click(coordinate_dict["首页point"][0], coordinate_dict["首页point"][1])
         time.sleep(1)
         for i in range(0, scroll_time):
-            pyautogui.moveTo(width/2, height/2)
+            pyautogui.moveTo(app_center_x, app_center_y)
             pyautogui.scroll(-100)
-        print(g_point)
         pyautogui.moveTo(g_point[0], g_point[1])
         pyautogui.click()
         time.sleep(1)
-        pyautogui.moveTo(coordinate_dict["委拍1point"][0], coordinate_dict["委拍1point"][1])
-        pyautogui.click()
-        time.sleep(1)
-        pyautogui.moveTo(coordinate_dict["全部point"][0], coordinate_dict["全部point"][1])
-        pyautogui.click()
-        time.sleep(1)
-        pyautogui.moveTo(coordinate_dict["委拍2point"][0], coordinate_dict["委拍2point"][1])
-        pyautogui.click()
-        time.sleep(2)
-        while True:
-            result = InferOcrApp(coordinate_dict["取消box"][0], coordinate_dict["取消box"][1], coordinate_dict["取消box"][2], coordinate_dict["取消box"][3])
-            try:
-                if result and result[0][0][1][0] == "取消":
-                    pyautogui.moveTo(coordinate_dict["全部point"][0], coordinate_dict["全部point"][1])
-                    pyautogui.click()
-                    time.sleep(0.25)
-                    pyautogui.moveTo(coordinate_dict["委拍2point"][0], coordinate_dict["委拍2point"][1])
-                    pyautogui.click()
-                    time.sleep(1)
-                else:
-                    print("委拍完了！！！")
-                    break
-            except:
-                print("委拍完了！")
-                break
+        sell_function(coordinate_dict, g_time)
+
+    time.sleep(1)
     pyautogui.moveTo(coordinate_dict["返回point"][0], coordinate_dict["返回point"][1])
+    time.sleep(1)
     pyautogui.click()
+    time.sleep(1)
     # 2. 如果刚才是委拍，返回我的，查看当前委拍数量，如果委拍数量与之前记录不一样，证明委拍成功
-    if wei_pai_num != 0:
-        while True:
+    if check_g_com_flag != 0:
+        pyautogui.moveTo(coordinate_dict["首页point"][0], coordinate_dict["首页point"][1])
+        pyautogui.click()
+        time.sleep(1)
+        pyautogui.moveTo(coordinate_dict["我的point"][0], coordinate_dict["我的point"][1])
+        pyautogui.click()
+        # 截取委拍位置的图像
+        result = InferOcrApp(coordinate_dict["委拍数量box"][0], coordinate_dict["委拍数量box"][1], coordinate_dict["委拍数量box"][2], coordinate_dict["委拍数量box"][3])
+        wei_pai_num_2 = int(result[0][-1][1][0])
+        if wei_pai_num_2 != wei_pai_num_1:
+            print("委拍成功，当前委拍数为 " + str(wei_pai_num_2))
+        else:
+            print("委拍失败，直接进参拍")
             pyautogui.moveTo(coordinate_dict["首页point"][0], coordinate_dict["首页point"][1])
             pyautogui.click()
             time.sleep(1)
-            pyautogui.moveTo(coordinate_dict["我的point"][0], coordinate_dict["我的point"][1])
+            for i in range(0, scroll_time):
+                pyautogui.moveTo(app_center_x, app_center_y)
+                pyautogui.scroll(-100)
+            print(g_point)
+            pyautogui.moveTo(g_point[0], g_point[1])
             pyautogui.click()
-            # 截取委拍位置的图像
-            result = InferOcrApp(coordinate_dict["委拍数量box"][0], coordinate_dict["委拍数量box"][1], coordinate_dict["委拍数量box"][2], coordinate_dict["委拍数量box"][3])
-            now_wei_pai_num = int(result[0][-1][1][0])
-            if now_wei_pai_num != wei_pai_num:
-                break
-
+            time.sleep(1)
+            buy_function(coordinate_dict, g_time)
+            return
         while True:
             # 2.1 如果委拍成功，不停的点返回，再点我的，直到委拍数量减少
             pyautogui.moveTo(coordinate_dict["首页point"][0], coordinate_dict["首页point"][1])
@@ -123,8 +93,8 @@ def BuySellFunction(g_com, g_time, g_point, scroll_time):
             time.sleep(1)
             pyautogui.moveTo(coordinate_dict["我的point"][0], coordinate_dict["我的point"][1])
             pyautogui.click()
-            result = InferOcrApp(coordinate_dict["委拍数量box"][0], coordinate_dict["委拍数量box"][1], coordinate_dict["委拍数量box"][2], coordinate_dict["委拍数量box"][3])
-            if now_wei_pai_num != int(result[0][-1][1][0]):
+            wei_pai_num_3 = InferOcrApp(coordinate_dict["委拍数量box"][0], coordinate_dict["委拍数量box"][1], coordinate_dict["委拍数量box"][2], coordinate_dict["委拍数量box"][3])
+            if wei_pai_num_3 != wei_pai_num_2:
                 print("委拍的物品已拍卖！")
                 break
         # 2.2 委拍完成，进参拍，拍完return
@@ -132,65 +102,39 @@ def BuySellFunction(g_com, g_time, g_point, scroll_time):
         pyautogui.click()
         time.sleep(1)
         for i in range(0, scroll_time):
-            pyautogui.moveTo(width / 2, height / 2)
+            pyautogui.moveTo(app_center_x, app_center_y)
             pyautogui.scroll(-100)
         print(g_point)
         pyautogui.moveTo(g_point[0], g_point[1])
         pyautogui.click()
         time.sleep(1)
-        pyautogui.moveTo(coordinate_dict["参拍1point"][0], coordinate_dict["参拍1point"][1])
-        pyautogui.click()
-        time.sleep(1)
-        pyautogui.moveTo(coordinate_dict["全部point"][0], coordinate_dict["全部point"][1])
-        pyautogui.click()
-        time.sleep(1)
-        pyautogui.moveTo(coordinate_dict["参拍2point"][0], coordinate_dict["参拍2point"][1])
-        pyautogui.click()
-        time.sleep(2)
-        while True:
-            result = InferOcrApp(coordinate_dict["取消box"][0], coordinate_dict["取消box"][1], coordinate_dict["取消box"][2], coordinate_dict["取消box"][3])
-            try:
-                if result and result[0][0][1][0] == "取消":
-                    pyautogui.moveTo(coordinate_dict["全部point"][0], coordinate_dict["全部point"][1])
-                    pyautogui.click()
-                    time.sleep(0.25)
-                    pyautogui.moveTo(coordinate_dict["参拍2point"][0], coordinate_dict["参拍2point"][1])
-                    pyautogui.click()
-                    time.sleep(1)
-                else:
-                    print("抢完了！！！")
-                    return
-            except:
-                print("抢完了！")
-                return
+        buy_function(coordinate_dict, g_time)
+        return
+
 def Shopping_time(g_com, g_time):
+    pyautogui.click(coordinate_dict["首页point"][0], coordinate_dict["首页point"][1])
+    time.sleep(1)
     # 2. 获取屏幕中模拟器截图
     scoll_time = 0
-    find_flag = False
-    g_com_p = (0,0)
     while True: # 添加循环，每次一循环滚轮滚一次
         result = InferOcrApp()
         for idx in range(len(result)):
             res = result[idx]
             for line in res:
-                if calculate_text_difference(g_com, line[-1][0]) >= 0.85:
+                if calculate_text_difference(g_com, line[-1][0]) >= 0.9:
                     # 找到了，返回边框中心点
-                    find_flag = True
                     g_com_p = calculate_center(line[0])
-        if find_flag:
-            # 进入抢货函数
-            BuySellFunction(g_com, g_time, g_com_p, scoll_time)
-            break
-        else:
-            if scoll_time > 3:
-                print(f"没有【{g_com}】这个商品！！")
-                pyautogui.moveTo(width / 2, height / 2)
-                pyautogui.scroll(500)
-                return
-            scoll_time += 1
-            pyautogui.moveTo(width/2, height/2)
-            pyautogui.scroll(-100)
-            time.sleep(2)
+                    BuySellFunction(g_com, g_time, g_com_p, scoll_time)
+                    return
+        if scoll_time > 3:
+            print(f"没有【{g_com}】这个商品！！")
+            pyautogui.moveTo(app_center_x, app_center_y)
+            pyautogui.scroll(500)
+            return
+        scoll_time += 1
+        pyautogui.moveTo(app_center_x, app_center_y)
+        pyautogui.scroll(-100)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
@@ -202,7 +146,9 @@ if __name__ == "__main__":
             shop_list.append((commodity, deadtime))
             last_com_time[commodity] = 0
     # 写入当前版本的各关键坐标文件
-    app_center_x = set_coordinate()
+    # app_center_x = set_coordinate()
+    # print(app_center_x)
+    app_center_x = 1440
     pyautogui.moveTo(app_center_x, app_center_y)
     # 加载坐标字典
     with open("coordinate_dict.txt", "r", encoding="utf8") as f:
@@ -229,7 +175,7 @@ if __name__ == "__main__":
             # print(G_time)
             # print(trans_time)
             # print(last_com_time[Com])
-            Shopping_time(Com, GotTime)
+            Shopping_time(Com, G_time)
             last_com_time[Com] = trans_time
         time.sleep(10)
 
